a31addbfa0f8efd2b4670d07a0453442b6ecbb06
thugperfect <meenamani9444@gmail.com>
2023-09-29 16:38:30 +0530

    compete

---
+++ script.js
@@ -0,0 +1,157 @@
+let righttriangle = ''
+for (i = 1; i < 6; i++) {
+    for (j=0;j<i;j++){
+        righttriangle +='*'
+    }
+    righttriangle += '\n'
+}
+console.log("Right Triangle :");
+console.log(righttriangle);
+
+let square = ''
+
+for (i = 1;i<6;i++){
+    for(j = 1;j<6;j++){
+        square += '*'
+
+    }
+    square+='\n'
+}
+console.log("Square :");
+console.log(square);
+
+let hollowSQ = ''
+
+for(i = 0;i<6;i++){
+    for(j=0;j<6;j++){
+
+        if(i === 0 || i === 5){
+            hollowSQ += '*'
+        }else{
+            if( j === 0 || j === 5){
+                hollowSQ += '*'
+            }
+            else{
+                hollowSQ += ' '
+            }
+        }
+    }
+    hollowSQ += '\n'
+}
+console.log("Hollow Square :");
+console.log(hollowSQ);
+
+let righttriangle2 = ''
+
+for(i = 0;i<6;i++){
+    for(j = 0;j<6-i;j++){
+        righttriangle2 += ' '
+    }
+    for(k = 0;k<i;k++){
+        righttriangle2+='*'
+    }
+    righttriangle2 += '\n'
+}
+console.log(righttriangle2);
+console.log(righttriangle2);
+
+
+let hollowtriangle = ''
+
+for(i = 0;i<10;i++){
+    for(j=0;j<i;j++){
+        if(i ===9){
+            hollowtriangle += "*"
+        }else{
+            if(j === 0  || j === i-1){
+                hollowtriangle +="*"
+            }
+            else{
+                hollowtriangle+=" "
+            }
+        }
+       
+     }
+
+     hollowtriangle+='\n'   
+}
+
+console.log('Hollow Triangle :');
+console.log(hollowtriangle);
+
+let hollowtriangle2 = ''
+
+for(i = 0;i<10;i++){
+    for(j = 0;j<10-i;j++){
+        hollowtriangle2 += ' '
+    }
+
+    for(k = 0;k<i;k++){
+
+        if(i === 9){
+            hollowtriangle2 +="*"
+        }else{
+            if(k === 0 || k === i-1){
+                hollowtriangle2 +='*'
+            }else{
+                hollowtriangle2 += ' '
+            }
+        }
+
+        
+    }
+    hollowtriangle2 +='\n'
+}
+console.log("hollow Triangle2 :");
+console.log(hollowtriangle2);
+
+let pyramid = ''
+
+for(i = 0;i<6;i++){
+    for(j = 0;j<6-i;j++){
+        pyramid += ' '
+    }
+    for(j = 0;j <2*i-1;j++){
+        pyramid+='+'
+    }
+    pyramid+='\n'
+}
+console.log("pyramid :");
+console.log(pyramid);
+
+let revpyramid = ''
+
+for(i = 0;i<6;i++){
+    for(j = 0;j<i;j++){
+        revpyramid +=' '
+    } 
+    for(k = 0;k< 2*(5-i)-1;k++){
+        revpyramid += "*"
+    }
+    revpyramid +='\n'
+
+}
+
+console.log(revpyramid);
+
+
+let hollowpyramid = ''
+for(i = 0;i<6;i++){
+    for(j = 0;j<6-i;j++){
+        hollowpyramid +=' '
+    }
+    for(k = 0;k<2*i-1;k++){
+        if(i === 5){
+            hollowpyramid+='*'
+        }else{
+            if(k === 0 || k ===2*i-2){
+                hollowpyramid+='*'
+            }
+            else{
+                hollowpyramid+=' '
+            }
+        }
+    }
+    hollowpyramid += '\n'
+}
+console.log(hollowpyramid);
